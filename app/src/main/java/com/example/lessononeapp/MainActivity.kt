255690b190d4fb930f89c571bd72b3d6fcf1512c
veenorth <ham0nug@gmail.com>
2024-10-15 23:41:26 +0900

    lesson2

--- app/src/main/java/com/example/lessononeapp/MainActivity.kt
+++ app/src/main/java/com/example/lessononeapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.lessononeapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,13 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val button = findViewById<Button>(R.id.button_1)
+
+        button.setOnClickListener {
+            val intent = Intent(this,MainActivity2::class.java)
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/example/lessononeapp/MainActivity2.kt
+++ app/src/main/java/com/example/lessononeapp/MainActivity2.kt
@@ -1,6 +1,7 @@
 package com.example.lessononeapp
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,6 @@ class MainActivity2 : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
     }
 }
